22dc2fa7ff3718ce1c903bd9ea123fb66267a8de
Tino <anthony1@gmail.com>
2022-11-14 11:40:27 -0500

    new

--- components/MovieIdContainer.js
+++ components/MovieIdContainer.js
@@ -4,21 +4,20 @@ function MovieIdContainer({ images, name, description }) {
   return (
     <>
       {images && (
-        <div className="max-w-7xl  mx-auto">
-          <div className="w-full  relative bg-black h-[500px]">
-            <Image
-              layout="fill"
-              className="object-fill"
-              src={`https://image.tmdb.org/t/p/w500${images}`}
-              alt=""
-            />
+        <div className="max-w-7xl mt-[10px] flex mx-auto">
+          <div>
+            <div className="  w-[400px]  bg-black  h-[400px] relative">
+              <Image
+                layout="fill"
+                src={`https://image.tmdb.org/t/p/w500${images}`}
+                alt=""
+              />
+            </div>
           </div>
 
-          <div className="  m-[30px]  top-[80px]">
-            <div className="text-[25px]  font-[Poppins] text-center ">
-              {name}
-            </div>
-            <p className="text-gray-400 w-[800px] my-[20px] mx-auto text-center font-[Poppins] ">
+          <div className="flex flex-col justify-center  ml-[15px] ">
+            <div className="text-[25px]  font-[Poppins]  ">{name}</div>
+            <p className="text-gray-400 w-[800px] my-[20px]  font-[Poppins] ">
               {description}
             </p>
           </div>
